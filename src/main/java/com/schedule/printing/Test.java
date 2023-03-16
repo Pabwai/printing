2e951cd1a3fe2e16a070a6c9f2830e3283c2c174
Pabwai <117081869+Pabwai@users.noreply.github.com>
2023-03-16 18:31:27 +0700

    fix

---
+++ src/main/java/com/schedule/printing/Test.java
@@ -0,0 +1,17 @@
+package com.schedule.printing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// Initialising String
+        String gfg = new String("barcod");
+ 
+        System.out.print(gfg.substring(0,6));
+ 
+        // Initial index of 'g' will print
+        // prints 11
+        //System.out.println(gfg.indexOf("barcode"));
+
+	}
+
+}
